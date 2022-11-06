2e751bdccd00b83d6833b198012b6523fdd1a627
nestor <nestordesabille4@gmail.com>
2022-11-06 14:40:20 +0800

    finish version 1

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function getPlayerChoice(){
-    const choices = ["rock","paper","scissors"];
-    let userChoice = prompt("Please choose either Rock, Paper, or Scissors: ");
-    userChoice = userChoice.toLowerCase();
-    if((choices.includes(userChoice))){
-        return userChoice;
-    }
-    else{
-        return null;
-    }
-    
-}
-
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random()*3);
     let comChoice = "";
@@ -26,29 +13,71 @@ function getComputerChoice(){
     return comChoice;
 }
 
+function showImage(imgID,chosen){
+    const img = document.getElementById(imgID);
+    img.src = "pictures/"+chosen+".png";
+}
+
 function playRound(playerSelection,computerSelection){
     let result = "";
     if(playerSelection==computerSelection){
-        result = "Round ended in draw";
+        result = 0;
     }
     else if(((playerSelection=="rock")&&(computerSelection=="scissors"))||
     ((playerSelection=="scissors")&&(computerSelection=="paper"))||
     ((playerSelection=="paper")&&(computerSelection=="rock"))){
-        result = "Player wins (Player: "+playerSelection+" vs Computer: "+computerSelection+")";
+        result = 1;
     }
     else{
-        result = "Computer wins (Player: "+playerSelection+" vs Computer: "+computerSelection+")";
+        result = 2;
     }
     return result;
 }
 
-function game(){
-    for(let i=1;i<=5;i++){
-        console.log("Round "+String(i));
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-    }
+function round(buttonId){
+    const playerSelection = buttonId;
+    showImage("player-img",playerSelection);
+
+    const computerSelection = getComputerChoice();
+    showImage("computer-img",computerSelection);
+
+    let currentWinner = playRound(playerSelection,computerSelection);
+
+    return currentWinner;
+    
 }
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let buttonId = 0;
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click',()=>{
+            buttonId = button.id;
+            if((playerScore<=5)&&(computerScore<=5)){
+                currentWinner = round(buttonId);
+                if(currentWinner==1){
+                    playerScore++;
+                    document.getElementById("player-score").innerText = playerScore.toString()
+                }
+                else if(currentWinner==2){
+                    computerScore++;
+                    document.getElementById("computer-score").innerText = playerScore.toString()
+                }
+            }
+            if(playerScore==5){
+                alert("You won! You proved that machines can't beat humans!!");
+            }
+            else if(computerScore==5){
+                alert("You lost! Machines will take over the world!!");
+            }
+            
+        });
+    });
+
+}
+
 
 game();
